d6f4beed4bfec70150a2b8b0d4243ebb7b5eb226
Francis Chong <francis@goodnotesapp.com>
2020-09-20 16:08:30 +0800

    implement tidy

--- Sources/SwiftTidy/SwiftTidy.swift
+++ Sources/SwiftTidy/SwiftTidy.swift
@@ -1,21 +1,66 @@
 import CTidy
+import Foundation
 
-class Document {
-    var document: TidyDoc!
-    var text: String { "Hello, World!" }
+public enum TidyError: Error {
+    case parse(Int, String)
+    case cleanAndRepair(Int, String)
+}
 
-    init() {
+extension CTidy.Bool {
+    static let no = CTidy.Bool(rawValue: 0)
+    static let yes = CTidy.Bool(rawValue: 1)
+}
+
+public class Document {
+    public static let defaultConfiguration: [String: String] = [
+        "wrap": "0",
+        "doctype": "omit",
+        "show-body-only": "auto",
+        "output-html": "yes"
+    ]
+
+    let document: TidyDoc!
+
+    public init(_ configuration: [String: String] = Document.defaultConfiguration) {
         document = tidyCreate()
-    }
-
-    func clean(_ html: String) throws {
-        tidySetInCharEncoding(document, "utf-8")
-        tidySetOutCharEncoding(document, "utf-8")
-
+        tidyOptSetBool(document, TidyForceOutput, .yes)
+        for (key, value) in configuration {
+            tidyOptParseValue(document, key, value)
+        }
     }
 
     deinit {
         tidyRelease(document)
-        document = nil
+    }
+
+    public func clean(_ html: String) throws -> String {
+        var errorBuffer: TidyBuffer = TidyBuffer()
+        tidyBufInit(&errorBuffer)
+        defer {
+            tidyBufFree(&errorBuffer)
+        }
+        tidySetErrorBuffer(document, &errorBuffer)
+
+        var outputBuffer: TidyBuffer = TidyBuffer()
+        tidyBufInit(&outputBuffer)
+        defer {
+            tidyBufFree(&outputBuffer)
+        }
+
+        tidySetInCharEncoding(document, "utf-8")
+        tidySetOutCharEncoding(document, "utf-8")
+
+        var returnCode = tidyParseString(document, html)
+        guard returnCode >= 0 else {
+            throw TidyError.parse(Int(returnCode), String(cString: errorBuffer.bp))
+        }
+
+        returnCode = tidyCleanAndRepair(document)
+        guard returnCode >= 0 else {
+            throw TidyError.cleanAndRepair(Int(returnCode), String(cString: errorBuffer.bp))
+        }
+
+        tidySaveBuffer(document, &outputBuffer)
+        return String(cString: outputBuffer.bp)
     }
 }
